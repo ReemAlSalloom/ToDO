7d69f0d343e67452458cecd8de7e49b5e3ed6870
Reem Alsalloom <r.alsalloom@gmail.com>
2019-01-17 12:37:25 +0300

    add project

--- ToDo/CheckListViewController.swift
+++ ToDo/CheckListViewController.swift
@@ -10,29 +10,76 @@ import UIKit
 
 class CheckListViewController: UITableViewController {
 
+    var row0 : checkListItem
+    required init?(coder aDecoder: NSCoder) {
+        row0 = checkListItem()
+        row0.text = "from init"
+        super.init(coder: aDecoder)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 50
     }
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath)
+        {
+            if indexPath.row == 0
+            {
+                row0.checked = true
+                cell.accessoryType = .none
+            }
+            else
+            {
+                cell.accessoryType = .checkmark
+        }
+            if cell.accessoryType == .none
+            {
+                cell.accessoryType = .checkmark
+                
+            }
+            else
+            {
+                cell.accessoryType = .none
+                
+            }
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "checkListItem", for: indexPath)
         
         if let label = cell.viewWithTag(1000) as? UILabel
         {
-           if indexPath.row == 0
+           if indexPath.row  == 0
            {
-            label.text = "Hi"
+            label.text = row0.text
            
             }
-            else
+            if indexPath.row % 5 == 1
+           {
+            label.text = "Hi 1"
+            
+            }
+            if indexPath.row % 5 == 2
+           {
+            label.text = "Hi 2"
+            
+            }
+            if indexPath.row % 5 == 3
+           {
+            label.text = "Hi 3"
+            
+            }
+             if indexPath.row % 5 == 4
            {
             
-            label.text = "hi 1 "
+            label.text = "hi 4 "
             }
             
         }
---
+++ ToDo/checkListItem.swift
@@ -0,0 +1,14 @@
+//
+//  checkListItem.swift
+//  ToDo
+//
+//  Created by Reem Saloom on 1/17/19.
+//  Copyright © 2019 Reem AlSalloom. All rights reserved.
+//
+
+import Foundation
+
+class checkListItem {
+    var text = ""
+    var checked = false
+}
